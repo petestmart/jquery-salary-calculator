26c75924b42f308d0742ca608c77b9232fc9c7cd
Pete St. Martin <peter.r.stmartin@gmail.com>
2019-04-13 10:37:32 -0500

    button defines variables and logs to console

--- script.js
+++ script.js
@@ -7,4 +7,19 @@ let employees = [];
 function readyNow() {
     console.log( 'JQ is R2G' );
     
+    storeInfo();
+
 }
+
+function storeInfo(){
+    console.log('in storeInfo');
+    $( '#submit' ).on( 'click', function()  {
+        console.log( 'submit button clicked' );
+        let firstNameTyped = $('#firstNameIn').val; //.val is a GETTER of deets
+        let lastNameTyped = $('#lastNameIn').val
+        let empIdTyped = $('#empIdIn').val
+        let jobTitleTyped = $('#jobTitleIn').val
+        let annualSalaryTyped = $('#annualSalaryIn').val
+    }) // end click submit
+    
+} // end storeInfo
