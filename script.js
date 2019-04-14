a8740698f568c884fbb681ab32f8d957e2f497ac
Pete St. Martin <peter.r.stmartin@gmail.com>
2019-04-14 11:05:23 -0500

    organized and cleaned up

--- script.js
+++ script.js
@@ -5,6 +5,42 @@ console.log( 'JS' );
 let employees = [];
 let totalAnnualSalaries = [];
 
+function clearInputs() {
+    // Clear the inputs; Setter, Irish
+    $('#firstNameIn').val('');
+    $('#lastNameIn').val('');
+    $('#empIdIn').val('');
+    $('#jobTitleIn').val('');
+    $('#annualSalaryIn').val('');
+}
+
+// function to add up annual salaries
+function calcSalary() {
+    console.log('in calcSalary');
+    let sum = x;
+    // loop through employees
+    for (let i = 0; i < employees.length; i++) {
+        if (employees.length < 1) {
+            let calcSecondary = (parseFloat(`${employees[i].annualSalary}`)
+            )
+        } // end if
+        else {
+            let calcSecondary = (parseFloat(`${employees[i].annualSalary}` += $('#annualSalaryIn').val()))
+        } // end else
+    } // end for loop
+    console.log(calcSecondary);
+    totalAnnualSalaries.push(calcSecondary);
+    monthlyAppend();
+} // end function calcSalary
+
+// function to append the new data to the DOM
+function monthlyAppend() {
+    $('#totalMonthly').empty();
+    for (let i = 0; i < totalAnnualSalaries.length; i++) {
+        // target totalMonthly, calculate, append total monthly to the DOM
+        $('#totalMonthly').append();
+    } //end for loop totalAnnualSalaries
+} // end function monthlyAppend
 
 function readyNow() {
     console.log( 'JQ is R2G' );
@@ -12,11 +48,11 @@ function readyNow() {
     storeInfo();
 
 }
+
 // take in information on button click and append it to the DOM
 function storeInfo(){
     $( '#submit' ).on( 'click', function()  {
         console.log( 'submit button clicked' );
-        document.getElementById("#annualSalaryIn").innerHTML = totalAnnualSalaries.reduce(calcSalary);
         // declare variables and link them to user data
         let firstNameTyped = $('#firstNameIn').val(); //.val is a GETTER of deets
         let lastNameTyped = $('#lastNameIn').val();
@@ -42,36 +78,9 @@ function storeInfo(){
             empID: empIdTyped,
             jobTitle: jobTitleTyped,
             annualSalary: annualSalaryTyped
-        }
+        } // end object employeeIn
         employees.push( employeeIn )
         
-
+    calcSalary();
     }) // end click submit
 } // end storeInfo
-
-// function to add up annual salaries
-function calcSalary( total, num ) {
-    return total + num;
-    // loop through employees
-    // totalAnnualSalaries.push( $('#annualSalaryIn').val() );
-    // } // end for loop
-    monthlyAppend();
-} // end function calcSalary
-
-// function to append the new data to the DOM
-function monthlyAppend() {
-    $('#totalMonthly').empty();
-    for (let i = 0; i < totalAnnualSalaries.length; i++){
-    // target totalMonthly, calculate, append total monthly to the DOM
-    $('#totalMonthly').append( );
-    } //end for loop totalAnnualSalaries
-} // end function monthlyAppend
-
-function clearInputs(){
-    // Clear the inputs; Setter, Irish
-    $('#firstNameIn').val('');
-    $('#lastNameIn').val('');
-    $('#empIdIn').val('');
-    $('#jobTitleIn').val('');
-    $('#annualSalaryIn').val('');
-}
